59b73e04ac150b0a0b28c436704185d9e688fa44
Vasco Godinho <vascogodinho88@live.com.pt>
2024-01-05 16:15:51 +0000

    aboutus

--- PokemonPartyPlanner/src/App.jsx
+++ PokemonPartyPlanner/src/App.jsx
@@ -3,14 +3,19 @@ import HomePage from "./Components/HomePage.jsx";
 import AboutUs from "./Components/AboutUs.jsx";
 import NotFound from "./Components/NotFoundPage.jsx";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar.jsx";
+import "./Styles/Navbar.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutUs />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
--- PokemonPartyPlanner/src/Components/AboutUs.jsx
+++ PokemonPartyPlanner/src/Components/AboutUs.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import Navbar from "./Navbar";
-import "../Styles/Navbar.css";
+import "../Styles/AboutUs.css";
+import { Link } from "react-router-dom";
+import gitHubLogo from "../assets/GitHub-Logo.png";
 
 function AboutUs() {
   return (
     <div>
-      <Navbar />
-
-      <div>
-        <h1>About Us</h1>
-        <p>This is the about us page content.</p>
+      <h1>About Us</h1>
+      <div className="vasco">
+        <h3>Vasco Godinho</h3>
+        <img src="./src/assets/Vasco.jpg"></img>
+      </div>
+      <div className="victor">
+        <h3>Victor Silva</h3>
+        <img src="./src/assets/Victor.jpg"></img>
+        <p>My name is Victor Silva, I am 24 years old.</p>
+        <p>
+          I was born in Brazil but came to Portugal when I was five years old,
+          since then I have lived in Lisbon. I have a background in Management,
+          but I have always been interested in technology, which is why I
+          decided not to pursue the area of economics. I really like playing
+          football and playing computer games.
+        </p>
+        <Link to="https://github.com/Vini1602" className="gitHub">
+          <img src={gitHubLogo} alt="Github Page"></img>
+        </Link>
       </div>
     </div>
   );
