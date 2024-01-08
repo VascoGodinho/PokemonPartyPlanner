70bbc67ddd21991f0dfea2c0f3dc2d936811faa3
Vasco Godinho <vascogodinho88@live.com.pt>
2024-01-08 16:17:01 +0000

    bug

--- PokemonPartyPlanner/src/Components/HomePage.jsx
+++ PokemonPartyPlanner/src/Components/HomePage.jsx
@@ -8,7 +8,6 @@ import CreatePokemonForm from "./CreatePokemonForm.jsx";
 
 function HomePage() {
   const [pokedexData, setPokedexData] = useState([]);
-  /* const [showCreateForm, setShowCreateForm] = useState(false); */
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,28 +26,12 @@ function HomePage() {
     fetchData();
   }, []);
 
-  /* const handleCreatePokemon = (newPokemon) => {
-    // Send the new Pokemon data to the server
-    axios.post("https://pokedexbackdendapi.adaptable.app/pokemons", newPokemon) // Adjust the URL for creating a new Pokemon
-      .then((response) => {
-        // Update the local state with the new Pokemon
-        setPokedexData((prevData) => [...prevData, response.data]);
-        // Hide the CreatePokemonForm after successful submission
-        setShowCreateForm(false);
-      })
-      .catch((error) => console.error('Error creating Pokemon:', error));
-  }; */
-
   return (
     <div>
       <div className="Navbar fixed-navbar">
         <Navbar />
       </div>
       <div className="App">
-        {/*    {/* Add a button or a link to display the CreatePokemonForm }
-        <button onClick={() => setShowCreateForm(true)}>Create Pokemon</button>
-        {/* Conditionally render the CreatePokemonForm based on user action }
-        {showCreateForm && <CreatePokemonForm onSubmit={handleCreatePokemon} />} */}
         {pokedexData.map((pokemon) => (
           <Link key={pokemon.id} to={`/pokemon/${pokemon.id}`}>
             <div className="pokemon-container">
